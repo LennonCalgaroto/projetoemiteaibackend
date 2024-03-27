4b12022ec5d331bf7099ed0f80f7e56fb6aba60e
Lennon Calgaroto <lennoncalgaroto95@gmail.com>
2024-03-26 23:05:16 -0300

    3

---
+++ src/main/java/com/projetoemiteai/projetoemiteai/PedidoProdutoService.java
@@ -0,0 +1,8 @@
+package com.projetoemiteai.projetoemiteai;
+
+import java.math.BigDecimal;
+
+public class PedidoProdutoService {
+    public void criarPedidoProduto(Long pedidoId, Long produtoId, Integer quantidade, BigDecimal valorTotalPedido) {
+    }
+}
