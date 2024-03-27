00a75fcddce0bacb1684237f57b1bc4a34b53a0d
Lennon Calgaroto <lennoncalgaroto95@gmail.com>
2024-03-26 23:03:27 -0300

    segundo commit

--- src/main/java/com/projetoemiteai/projetoemiteai/PedidoController.java
+++ src/main/java/com/projetoemiteai/projetoemiteai/PedidoController.java
@@ -1,4 +1,32 @@
 package com.projetoemiteai.projetoemiteai;
 
-public class PedidoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/pedido-produto")
+public class PedidoProdutoController {
+
+    private final PedidoProdutoService pedidoProdutoService;
+
+    @Autowired
+    public PedidoProdutoController(PedidoProdutoService pedidoProdutoService) {
+        this.pedidoProdutoService = pedidoProdutoService;
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarPedidoProduto(@RequestParam Long pedidoId,
+                                                     @RequestParam Long produtoId,
+                                                     @RequestParam Integer quantidade,
+                                                     @RequestParam BigDecimal valorTotalPedido) {
+        try {
+            pedidoProdutoService.criarPedidoProduto(pedidoId, produtoId, quantidade, valorTotalPedido);
+            return new ResponseEntity<>("PedidoProduto criado com sucesso.", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao criar PedidoProduto: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
